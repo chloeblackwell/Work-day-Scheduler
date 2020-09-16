96bbaec1463d28e779a05d74b716a8f837ebd841
Chloe Blackwell <chloeblackwell2@hotmail.com>
2020-09-16 21:30:20 +1000

    Made some changes to come files

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+// Displays current day on the screen
 $ ("#currentDay").text(moment().format('MMMM Do YYYY'));
 
-var currentHour = (moment().format('H'));
+// Works out the current hour 
+var currentHour = (moment().format('h'));
 var hourBlock = $('.hour').text();
 
-// Add colour to the hoour blocks to show present, past anf future 
-function addColour() {
-    if (hourBlock === currentHour) {
-        $('.userActivity').addClass('.present')
-    } else if (hourBlock < currentHour) {
-        $('.userActivity').addClass('.past')
+
+var nine = document.querySelector("#input1");
+var ten = document.querySelector("#input2");
+var eleven = document.querySelector("#input3");
+var twelve = document.querySelector("#input4");
+var one = document.querySelector("#input5");
+var two = document.querySelector("#input6");
+var three = document.querySelector("#input7");
+var four = document.querySelector("#input8");
+var five = document.querySelector("#input9");
+
+// Click function for the hour block 
+
+$('.saveBtn').on('click', function () {
+    
+});
+
+
+
+// Add colour to the hour blocks to show present, past anf future 
+
+function addColour () {
+
+    if (hourBlock < currentHour) {
+        $('.description').addClass("past");
+    } else if (hourBlock === currentHour) {
+        $('.description').removeClass("past");
+        $('.description').addClass("present");
+        
     } else {
-        $('.userActivity').addClass('.future')
+        $('.description').removeClass("past");
+        $('.description').removeClass("present");
+        $('.description').addClass("future");
     }
-};
+}
 
-// Local storage for activities 
+addColour();
 
-var userSchedule = $('.userActivity').value
-localStorage.setItem('schedule' , 'userActvity');
-$('.userActivity').value = localStorage.getItem('schedule' , 'userActivity')
+
+// 
+
+
+
